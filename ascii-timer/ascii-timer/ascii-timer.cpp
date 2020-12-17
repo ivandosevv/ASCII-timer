45741832c5013e9598252a49683bffc5ef443f77
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2020-12-17 19:00:02 +0200

    Added cpp file.

---
+++ ascii-timer/ascii-timer/ascii-timer.cpp
@@ -0,0 +1,10 @@
+// ascii-timer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is the beginning of the ASCII timer!\n";
+}
+
