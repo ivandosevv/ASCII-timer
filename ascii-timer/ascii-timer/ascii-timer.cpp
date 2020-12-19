6a39198e6f8ed6652fe9c582d9277009eed59677
yeetlord420 <solazy@abv.bg>
2020-12-19 13:55:08 +0200

    Created working timer without ASCII art
    Updated cpp file to working timer.

--- ascii-timer/ascii-timer/ascii-timer.cpp
+++ ascii-timer/ascii-timer/ascii-timer.cpp
@@ -2,9 +2,51 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "This is the beginning of the ASCII timer!\n";
+    int startingInput;
+
+    while (!(cin>> startingInput) || startingInput < 0)
+    {
+        cout << "Input a positive integer!\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int minutes, seconds;
+    minutes = startingInput / 60;
+    seconds = startingInput % 60;
+
+    while (minutes >= 0 && seconds >= 0)
+    {
+        cout << minutes << ":";
+        if (seconds < 10)
+        {
+            cout << "0";
+        }
+        cout << seconds << endl;
+
+        if (seconds == 0)
+        {
+            minutes--;
+            seconds = 59;
+        }
+        else
+        {
+            seconds--;
+        }
+
+        std::chrono::milliseconds timespan(1000);
+        std::this_thread::sleep_for(timespan);
+    }
+    
+    
+    cout << '\a';
+    system("pause");
 }
 
