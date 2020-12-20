eb38c568bf7fef62bcf501a98cf7438c736eae02
yeetlord420 <solazy@abv.bg>
2020-12-20 23:57:36 +0200

    Added a few ASCII numbers and tested them.
    Added first four ASCII numbers and tested to see how they look in the console.

--- ascii-timer/ascii-timer/ascii-timer.cpp
+++ ascii-timer/ascii-timer/ascii-timer.cpp
@@ -7,6 +7,12 @@
 
 using namespace std;
 
+void WhichAsciiToUse(int number, int row);
+void ASCII_One(int row);
+void ASCII_Two(int row);
+void ASCII_Three(int row);
+void ASCII_Four(int row);
+
 int main()
 {
     int startingInput;
@@ -24,12 +30,22 @@ int main()
 
     while (minutes >= 0 && seconds >= 0)
     {
+        // Cleans console.
+        system("cls");
+
         cout << minutes << ":";
         if (seconds < 10)
         {
             cout << "0";
         }
         cout << seconds << endl;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            WhichAsciiToUse(minutes, i);
+            WhichAsciiToUse(seconds, i);
+            cout << endl;
+        }
 
         if (seconds == 0)
         {
@@ -40,6 +56,7 @@ int main()
         {
             seconds--;
         }
+        
 
         std::chrono::milliseconds timespan(1000);
         std::this_thread::sleep_for(timespan);
@@ -48,5 +65,89 @@ int main()
     
     cout << '\a';
     system("pause");
+}
+
+void WhichAsciiToUse(int number, int row)
+{
+    switch (number)
+    {
+        //case 0: ASCII_Zero()
+    case 1: ASCII_One(row); break;
+    case 2: ASCII_Two(row); break;
+    case 3: ASCII_Three(row); break;
+    case 4: ASCII_Four(row); break;
+        default: 
+        {
+
+        }
+    }
+}
+
+void ASCII_One(int row)
+{
+    switch (row)
+    {
+        case 1: cout << "  @@@"; break;
+        case 2: cout << " @@@@"; break;
+        case 3: cout << "@@@!!"; break;
+        case 4: cout << "  !@!"; break;
+        case 5: cout << "  @!@"; break;
+        case 6: cout << "  !@!"; break;
+        case 7: cout << "  !!:"; break;
+        case 8: cout << "  :!:"; break;
+        case 9: cout << "  :::"; break;
+        case 10:cout << "   ::"; break;
+    }
+}
+
+void ASCII_Two(int row)
+{
+    switch (row)
+    {
+    case 1: cout << " @@@@@@ "; break;
+    case 2: cout << "@@@@@@@@"; break;
+    case 3: cout << "     @@@"; break;
+    case 4: cout << "    @!@ "; break;
+    case 5: cout << "   !!@  "; break;
+    case 6: cout << "  !!:   "; break;
+    case 7: cout << " !:!    "; break;
+    case 8: cout << ":!:     "; break;
+    case 9: cout << ":: :::::"; break;
+    case 10:cout << ":: : :::"; break;
+    }
+}
+
+void ASCII_Three(int row)
+{
+    switch (row)
+    {
+    case 1: cout << "@@@@@@ "; break;
+    case 2: cout << "@@@@@@@"; break;
+    case 3: cout << "    @@@"; break;
+    case 4: cout << "    @!@"; break;
+    case 5: cout << "@!@!!@ "; break;
+    case 6: cout << "!!@!@! "; break;
+    case 7: cout << "    !!:"; break;
+    case 8: cout << "    :!:"; break;
+    case 9: cout << ":: ::::"; break;
+    case 10:cout << " : : : "; break;
+    }
+}
+
+void ASCII_Four(int row)
+{
+    switch (row)
+    {
+    case 1: cout << "     @@@ "; break;
+    case 2: cout << "    @@@@ "; break;
+    case 3: cout << "   @@!@! "; break;
+    case 4: cout << "  !@!!@! "; break;
+    case 5: cout << " @!! @!! "; break;
+    case 6: cout << "!!!  !@! "; break;
+    case 7: cout << ":!!:!:!!:"; break;
+    case 8: cout << "!:::!!:::"; break;
+    case 9: cout << "     ::: "; break;
+    case 10:cout << "     ::: "; break;
+    }
 }
 
