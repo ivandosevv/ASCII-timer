be4bd5429874e3e03e9a4dd0b86c6e02e6f78b21
yeetlord420 <solazy@abv.bg>
2020-12-23 18:26:04 +0200

    Centered timer and added a welcome message.
    The project is working just fine at this point. Will probably add some extra features, but I don't think there is anything else I can do on the timer itself.

--- ascii-timer/ascii-timer/ascii-timer.cpp
+++ ascii-timer/ascii-timer/ascii-timer.cpp
@@ -4,12 +4,15 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <iomanip>
 
 using namespace std;
 
 bool NumberHasFourDigits(int number);
+int NumberOfDigitsInNumber(int number);
 void InputWhileCorrect(int& number);
 void WhichAsciiToUse(int number, int row);
+void CenterHeight();
 void ASCII_Zero(int row);
 void ASCII_One(int row);
 void ASCII_Two(int row);
@@ -21,16 +24,24 @@ void ASCII_Seven(int row);
 void ASCII_Eight(int row);
 void ASCII_Nine(int row);
 void PrintColon(int row);
+void PrintWelcomeMessage();
 
+const int MAX_ROWS = 25;
+const int MAX_COLS = 80;
 
 int main()
 {
+    system("MODE 80,25");
+    
+    PrintWelcomeMessage();
+
     int startingInput;
     InputWhileCorrect(startingInput);
 
     while (!NumberHasFourDigits(startingInput))
     {
-        cout << "Input must have exactly four digits.\n";
+        system("cls");
+        cout << "\n\n\n\n\n" << setw(55) << "Input must have exactly four digits: ";
         InputWhileCorrect(startingInput);
     }
 
@@ -43,23 +54,41 @@ int main()
         // Cleans console.
         system("cls");
 
-        cout << minutes << ":";
+        //cout << minutes << ":";
         if (seconds < 10)
         {
-            cout << "0";
+            //cout << "0";
         }
-        cout << seconds << endl;
-
+        //cout << seconds << endl;
+        
+        CenterHeight();
         for (int i = 1; i <= 10; i++)
         {
             //cout << minutes / 10 << endl;
             //cout << minutes % 10 << endl;
             //cout << seconds / 10 << endl;
             //cout << seconds % 10 << endl;
-            WhichAsciiToUse(minutes / 100, i);
-            cout << " ";
-            WhichAsciiToUse(minutes % 100 / 10, i);
-            cout << " ";
+            
+            int numberOfDigitsInMinutes = NumberOfDigitsInNumber(minutes);
+            if (numberOfDigitsInMinutes == 3)
+            {
+                const int TIMER_SIZE = 58;
+                cout << setw(TIMER_SIZE - (MAX_COLS/2));
+                WhichAsciiToUse(minutes / 100, i);
+                cout << " ";
+                WhichAsciiToUse(minutes % 100 / 10, i);
+                cout << " ";
+            }
+            else if (numberOfDigitsInMinutes == 2)
+            {
+                cout << setw(26);
+                WhichAsciiToUse(minutes / 10, i);
+                cout << " ";
+            }
+            else
+            {
+                cout << setw(30);
+            }
             WhichAsciiToUse(minutes % 10, i);
             PrintColon(i);
             WhichAsciiToUse(seconds / 10, i);
@@ -78,7 +107,7 @@ int main()
             seconds--;
         }
         
-
+        CenterHeight();
         std::chrono::milliseconds timespan(1000);
         std::this_thread::sleep_for(timespan);
     }
@@ -98,15 +127,39 @@ bool NumberHasFourDigits(int number)
     }
 
     return (counter == 4) ? true : false;
+    //return true;
+}
+
+int NumberOfDigitsInNumber(int number)
+{
+    int counter = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        counter++;
+    }
+
+    return counter;
 }
 
 void InputWhileCorrect(int& number)
 {
+    const int PIXELS_DEEP = 50;
     while (!(cin >> number) || number < 0)
     {
-        cout << "Input a positive integer with exactly four digits.\n";
+        system("cls");
+        cout << "\n\n\n\n\n" << setw(60) << "Input a positive integer with exactly four digits:";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
+
+void CenterHeight()
+{
+    const int ROWS_TO_SKIP = (MAX_ROWS - 10) / 2;
+    for (int i = 0; i < ROWS_TO_SKIP; i++)
+    {
+        cout << endl;
     }
 }
 
@@ -131,16 +184,16 @@ void ASCII_Zero(int row)
 {
     switch (row)
     {
-    case 1: cout << " #$#$#$#$  "; break;
-    case 2: cout << "#$#$#$#$#$#"; break;
-    case 3: cout << "$#$     $#$"; break;
-    case 4: cout << "#$#     #$#"; break;
-    case 5: cout << "$#$     $#$"; break;
-    case 6: cout << "#$#     #$#"; break;
-    case 7: cout << "$#$     $#$"; break;
-    case 8: cout << "#$#     #$#"; break;
-    case 9: cout << "$#$#$#$#$#$"; break;
-    case 10:cout << " $#$#$#$#$ "; break;
+	case 1: cout << " #$#$#$#$  "; break;
+	case 2: cout << "#$#$#$#$#$#"; break;
+	case 3: cout << "$#$     $#$"; break;
+	case 4: cout << "#$#     #$#"; break;
+	case 5: cout << "$#$     $#$"; break;
+	case 6: cout << "#$#     #$#"; break;
+	case 7: cout << "$#$     $#$"; break;
+	case 8: cout << "#$#     #$#"; break;
+	case 9: cout << "$#$#$#$#$#$"; break;
+	case 10:cout << " $#$#$#$#$ "; break;
     }
 }
 
@@ -148,16 +201,16 @@ void ASCII_One(int row)
 {
     switch (row)
     {
-        case 1: cout << "     $$  "; break;
-        case 2: cout << "    $#$  "; break;
-        case 3: cout << "   $#$#  "; break;
-        case 4: cout << "  $# #$  "; break;
-        case 5: cout << " $#  $#  "; break;
-        case 6: cout << "$#   #$  "; break;
-        case 7: cout << "     $#  "; break;
-        case 8: cout << "     #$  "; break;
-        case 9: cout << "     $#  "; break;
-        case 10:cout << "   #$#$#$"; break;
+	case 1: cout << "       $$  "; break;
+	case 2: cout << "      $#$  "; break;
+	case 3: cout << "     $#$#  "; break;
+	case 4: cout << "    $# #$  "; break;
+	case 5: cout << "   $#  $#  "; break;
+	case 6: cout << "  $#   #$  "; break;
+	case 7: cout << "       $#  "; break;
+	case 8: cout << "       #$  "; break;
+	case 9: cout << "       $#  "; break;
+	case 10:cout << "     #$#$#$"; break;
     }
 }
 
@@ -313,3 +366,16 @@ void PrintColon(int row)
     case 10:cout << "   "; break;
     }
 }
+
+
+void PrintWelcomeMessage()
+{
+    cout << "_                _  _____  __     /~~~~~~\\/******\\    \\\\   //    __________" << endl;
+    cout << "\\\\              //||    |||~|     |/-----|| #### |    //   \\\\    |/////////\\" << endl;
+    cout << " \\\\     ~~     // ||      |~|     ||      | |  | |   /\\\\   //\\   |\\\\_____" << endl;
+    cout << "  \\\\   //\\\\   //  ||~~~~~>|~|     ||      | |  | |  // \\\\ // \\\\  |///////\\" << endl;
+    cout << "   \\\\ //  \\\\ //   ||      |~|____ |\\-----|| #### | //   \\//   \\\\ |\\\\_______" << endl;
+    cout << "    \\//    \\\\/    ||____|||______|\\~~~~~~/\\******///           \\\\|/////////\\" << endl;
+    cout << "\n\n" << setw(50) << "Input a 4 digit number: ";
+}
+
